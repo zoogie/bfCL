855271fe2af6698771617b0b9593c530d7533f33
Mike15678 <12465142+Mike15678@users.noreply.github.com>
2018-05-29 01:33:31 -0400

    Make sure output buffers are flushed

--- ocl_brute.c
+++ ocl_brute.c
@@ -372,6 +372,7 @@ int ocl_brute_msky(const cl_uint *msky, const cl_uint *ver, cl_uint msky_offset)
 		int msky3_offset = (j & 1 ? 1 : -1) * ((j + 1) >> 1);
 		cl_uint msky3 = msky[3] + msky3_offset;
 		printf("msed3:%08x offset:%d    \r", msky3, msky3_offset);
+		fflush(stdout);
 		for (i = 0; i < loops; ++i) {
 			cl_uint msky2 = i << group_bits;
 			OCL_ASSERT(clSetKernelArg(kernel, 2, sizeof(cl_uint), &msky2));
@@ -493,7 +494,7 @@ int ocl_brute_lfcs(cl_uint lfcs_template, cl_ushort newflag, const cl_uint *ver,
 		int fan = (j & 1 ? 1 : -1) * ((j + 1) >> 1);
 
 		if(fan > 0){                                         //check to see if bf exhausted in both directions, quit if so
-			if( lfcs_block + fan  > upper_bound  &&  (int)lfcs_block - fan < lower_bound){ 
+			if( lfcs_block + fan  > upper_bound  &&  (int)lfcs_block - fan < lower_bound){
 				printf("Exhausted all possible lfcs combinations, exiting ...\n\n");
 				break;
 			}
@@ -504,6 +505,7 @@ int ocl_brute_lfcs(cl_uint lfcs_template, cl_ushort newflag, const cl_uint *ver,
 		}
 
 		printf("%d \r", fan);
+		fflush(stdout);
 		for (i = 0; i < loops; ++i) {
 			cl_uint lfcs = lfcs_template + fan * 0x10000 + (i << (group_bits - 16));
 			OCL_ASSERT(clSetKernelArg(kernel, 0, sizeof(cl_uint), &lfcs));
@@ -531,13 +533,13 @@ int ocl_brute_lfcs(cl_uint lfcs_template, cl_ushort newflag, const cl_uint *ver,
 					printf("movable_part1.sed dumped to file\n");
 					printf("don't you dare forget to add the id0 to it!\n");
 				}
-			
+
 				printf("done.\n\n");
 				break;
 			}
 
 		}
-		
+
 		if (out) {
 			break;
 		}
@@ -559,4 +561,3 @@ int ocl_brute_lfcs(cl_uint lfcs_template, cl_ushort newflag, const cl_uint *ver,
 	clReleaseContext(context);
 	return !out;
 }
-
